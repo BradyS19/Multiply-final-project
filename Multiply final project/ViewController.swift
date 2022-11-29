a10ed3be7caa033224732539b9408689274a8f1e
brady scruton <bscru062@d127.org>
2022-11-29 09:19:15 -0600

    updated image code

--- Multiply final project/ViewController.swift
+++ Multiply final project/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLable: UILabel!
     @IBOutlet weak var answerImage: UIImageView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
